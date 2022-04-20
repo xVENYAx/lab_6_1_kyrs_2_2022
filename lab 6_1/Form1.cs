9c3994c93d02c9bf134576c956fb44da39db6c63
Sasha <t3t3t3@ukr.net>
2022-04-20 10:49:45 +0300

    Добавьте файлы проекта.

---
+++ lab 6_1/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace lab_6_1
+{
+    public partial class Form1 : Form
+    {
+        Mutex mut = new Mutex();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Control.CheckForIllegalCrossThreadCalls = false; // вимикаємо перевірку в Debug
+
+            for (int i = 0; i < 10; i++)
+            {
+                Thread myThread = new Thread(changeLabel);
+                string name = i.ToString();
+                myThread.Start(name);
+            }
+        }
+
+        void changeLabel(object obj)
+        {
+            while (true)
+            {
+                if (obj is string str)
+                {
+                    mut.WaitOne();
+
+                    label1.Text = str;
+                    int seed = Convert.ToInt32(str);
+                    Random fixRand = new Random(seed);
+                    label1.BackColor = Color.FromArgb(fixRand.Next(255), fixRand.Next(255), fixRand.Next(255));
+
+
+                    Thread.Sleep(1000);
+
+                    mut.ReleaseMutex();
+                }
+            }
+        }
+    }
+}
